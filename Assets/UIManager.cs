e49bbe002bd7334165ffecb356b89b7319c2de96
Agustin Hofmann <agustinhofmann@gmail.com>
2025-05-05 09:03:42 -0300

    a

---
+++ Assets/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class UIManager : MonoBehaviour
+{
+
+    public TextMeshProUGUI txtSaludo;
+
+    void Start()
+    {
+        txtSaludo.text = string.Empty;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
